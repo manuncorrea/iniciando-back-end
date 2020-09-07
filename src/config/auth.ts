c159b5a9794cd2076a72ec66c3fff3291142b44e
Emanuele Correa <manuncorrea@hotmail.com>
2020-09-07 17:14:49 -0300

    Rotas autenticadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '8d951838f3079df0deb2772ed0d1eaa2',
+        expiresIn: '1d',
+    },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,8 @@
 import {getRepository} from 'typeorm';
-import { compare } from 'bcryptjs'
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -8,7 +11,8 @@ interface Request {
 }
 
 interface Response {
-    user: User
+    user: User,
+    token: string
 }
 
 class AuthenticateUserService {
@@ -27,10 +31,16 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination');
         }
 
-        // Usuário autenticado
+        const {secret, expiresIn} = authConfig.jwt;
+
+        const token = sign ({}, secret, {
+            subject: user.id,
+            expiresIn,
+        });
 
         return {
             user,
+            token,
         };
     }
 }
