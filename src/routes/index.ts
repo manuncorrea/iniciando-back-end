c706a5cccede3ce62c0563c40fec2cfb6812807f
Emanuele Correa <manuncorrea@hotmail.com>
2020-09-07 10:41:57 -0300

    valiando as credencias

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import appointmentsRouter from './appointments.routes';
 
+import appointmentsRouter from './appointments.routes';
 import userRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
+import sessionRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionRouter);
 
 export default routes;
