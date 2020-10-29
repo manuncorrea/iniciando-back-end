367e4ec1d353bfab1386c21a96a63844a11eb8f5
Emanuele Correa <manuncorrea@hotmail.com>
2020-10-29 14:33:54 -0300

    template de emails

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,12 +6,20 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealEmailProvider from './MailProvider/implementations/EtherealEmailProvider';
 
+import IMailTemplateProvider from './MailTamplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTamplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
     'StorageProvider', 
     DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+    'MailTemplateProvider', 
+    HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider> (
     'MailProvider',
-    new EtherealEmailProvider(),
+    container.resolve(EtherealEmailProvider),
 );
