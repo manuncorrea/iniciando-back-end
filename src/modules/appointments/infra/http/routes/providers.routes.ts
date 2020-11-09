98214f63078cd4538412fabc3375b53577621d25
Emanuele Correa <manuncorrea@hotmail.com>
2020-11-09 12:11:09 -0300

    variaveis ambiente

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProviderController from '../controllers/ProviderController';
@@ -14,7 +15,25 @@ const providerDayAvaliabillyController = new ProviderDayAvaliabillyController();
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providerController.index);
-providersRouter.get('/:provider_id/month-availability', providerMonthAvaliabillyController.index);
-providersRouter.get('/:provider_id/day-availability', providerDayAvaliabillyController.index);
+
+providersRouter.get(
+    '/:provider_id/month-availability',
+     celebrate({
+         [Segments.PARAMS]: {
+             provider_id: Joi.string().uuid().required(),
+         },
+     }),
+     providerMonthAvaliabillyController.index
+     );
+
+providersRouter.get(
+    '/:provider_id/day-availability', 
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }),
+    providerDayAvaliabillyController.index
+    );
 
 export default providersRouter;
