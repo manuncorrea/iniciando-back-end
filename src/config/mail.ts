3ab5f2ff31481788d36e902b05c05f828278106c
Emanuele Correa <manuncorrea@hotmail.com>
2020-11-10 17:56:59 -0300

    emails pelo amazon ses

---
+++ src/config/mail.ts
@@ -0,0 +1,23 @@
+interface IMailConfig {
+    driver: 'ethereal' | 'ses';
+
+    defaults: {
+        from: {
+            email: string;
+            name: string;
+        };
+    };
+}
+
+export default {
+    driver: process.env.MAIL_DRIVER || 'ethereal',
+
+    defaults: {
+        from:{
+            email: 'contato@agenciae7.com',
+            name: 'Emanuele Correa',
+        },
+    },
+} as IMailConfig;
+
+//17:13
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealEmailProvider from './MailProvider/implementations/EtherealEmailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 import IMailTemplateProvider from './MailTamplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTamplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -21,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider> (
     'MailProvider',
-    container.resolve(EtherealEmailProvider),
+    mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealEmailProvider)
+    : container.resolve(SESMailProvider),
 );
